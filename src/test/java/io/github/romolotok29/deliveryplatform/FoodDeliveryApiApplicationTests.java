003f7f846fc0258f847ab66541600b44fe97c442
rmolotok <roland.from.laptop@gmail.com>
2026-08-12 16:31:53 +0300

    Initial Spring Boot project setup.

---
+++ src/test/java/io/github/romolotok29/deliveryplatform/FoodDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.romolotok29.deliveryplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
